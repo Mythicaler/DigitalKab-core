b1db623b518086495f1df4b5a072b150f9d51655
seniordev32 <senior.dev32@gmail.com>
2018-11-30 15:07:23 +0000

    Update slow-hash

--- src/simplewallet/simplewallet.h
+++ src/simplewallet/simplewallet.h
@@ -131,7 +131,7 @@ namespace cryptonote
     epee::console_handlers_binder m_cmd_binder;
 
     std::unique_ptr<tools::wallet2> m_wallet;
-    epee::net_utils::http::http_simple_client m_http_client;
+    net_utils::http::http_simple_client m_http_client;
     refresh_progress_reporter_t m_refresh_progress_reporter;
   };
 }
